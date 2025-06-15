b890cbbcd9ee3679bd4fc7be9c050dad8e0510d9
denskrlv <d.krylov@student.utwente.nl>
2025-06-15 23:36:51 +0200

    Add dataset download functionality to prepare.py

--- prepare.py
+++ prepare.py
@@ -1,5 +1,10 @@
+import kagglehub
 from compose_glide import ComposeGlide
 
 
 compose_glide = ComposeGlide(verbose=True)
 print(compose_glide)
+
+
+path = kagglehub.dataset_download("kushsheth/face-vae")
+print("Path to dataset files:", path)
